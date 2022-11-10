dbb8003913a8c02412a69b725189565854fc56f9
David <pairing4good@gmail.com>
2022-11-10 09:19:42 -0600

    updated tests and readme

--- README.md
+++ README.md
@@ -1,21 +1,12 @@
-# 028 Grid Layout
+# 001 Comments
 
-[![028 Grid Layout](https://img.youtube.com/vi/8U8jRi7lsfw/0.jpg)](https://www.youtube.com/watch?v=8U8jRi7lsfw)
+[![001 Comments](https://img.youtube.com/vi/fH7Fx-tlfxU/0.jpg)](https://www.youtube.com/watch?v=fH7Fx-tlfxU)
 
-The `index.html` file
-- should add styling so that the first-item grid area is named header
-- should add styling so that the second-item grid area is named leftNav
-- should add styling so that the third-item grid area is named main
-- should add styling so that the grid-container grid area is displayed as a grid
-- should add styling so that the grid-container grid area has a 10px gap
-- should add styling so that the grid-container grid area has a left navigation bar that extends vertially across the first and second row of the grid
-- should add styling so that the grid-container grid area has a header that extends horizontally across the top row
-- should add styling so that the grid-container grid area has a header that is twice the width of the left navigation bar
-- should add styling so that the grid-container grid area has a main section that extends horizontally across the second row
-- should add styling so that the grid-container grid area has a main section that is twice the width of the left navigation bar
+the javascript in the script element
+- should contain a single line comment that comments out line 14  (total = total - 2;)
+- should contain a multi-line comment that comments out lines 16, 17 and 18 (`total = total - 4;`, `total = total - 5;` and `total = total - 6;`)
+- should set the result element to 39
 
-Resources:
-- https://www.w3schools.com/css/css_grid.asp
+resources:
 
-Building toward CSTA Standards:
-- Develop programs for multiple computing platforms. (3B-AP-19) https://www.csteachers.org/page/standards
+https://www.w3schools.com/js/js_comments.asp
--- test/verify.test.js
+++ test/verify.test.js
@@ -37,102 +37,28 @@ afterEach(async () => {
   await browser.close();
 });
 
-describe('the first-item grid area', () => {
-    it('should be named header', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match(/\.first-item.*{[\s\S][^}]*grid-area.*:.*header.*;/g).length;
-      });
-      
-      expect(matches).toEqual(1);
+describe('the javascript in the script element', () => {
+  it('should contain a single line comment that comments out line 14', async () => {
+    const matches = await page.$eval('body script', (script) => {
+      return script.innerHTML.match(/\/\/.*total = total - 2;/g).length
     });
-});
+    
+    expect(matches).toBe(1);
+  });
 
-describe('the second-item grid area', () => {
-    it('should be named leftNav', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match(/\.second-item.*{[\s\S][^}]*grid-area.*:.*leftNav.*;/g).length;
-      });
+  it('should contain a multi-line comment that comments out lines 16, 17 and 18', async () => {
+    const matches = await page.$eval('body script', (script) => {
+      return script.innerHTML.match(/\/\*[\s\S][^\*\/]*total = total - 4;[\s\S][^\*\/]*total = total - 5;[\s\S][^\*\/]*total = total - 6;[\s\S]*\*\//g).length
+    });  
       
-      expect(matches).toEqual(1);
-    });
-});
+    expect(matches).toBe(1);
+  });
 
-describe('the third-item grid area', () => {
-    it('should be named main', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match(/\.third-item.*{[\s\S][^}]*grid-area.*:.*main.*;/g).length;
-      });
-      
-      expect(matches).toEqual(1);
+  it('should set the result element to 39', async () => {
+    const result = await page.$eval('#result', (result) => {
+      return result.innerHTML;
     });
-});
-
-describe('the grid-container grid area', () => {
-    it('should be displayed as a grid', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match(/\.grid-container.*{[\s\S][^}]*display.*:.*grid.*;/g).length;
-      });
       
-      expect(matches).toEqual(1);
-    });
-});
-
-describe('the grid-container grid area', () => {
-    it('should have a 10px gap', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match(/\.grid-container.*{[\s\S][^}]*gap.*:.*10px.*;/g).length;
-      });
-      
-      expect(matches).toEqual(1);
-    });
-});
-
-describe('the grid-container grid area', () => {
-    it('should have a left navigation bar that extends vertially across the first and second row of the grid', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match( /\.grid-container.*{[\s\S][^}]*display:.*grid.*;[\s\S][^}]*grid-template-areas.*:[\s\S][^}]*'leftNav.*'[\s\S][^}]*'leftNav.*'.*;/g).length;
-      });
-      
-      expect(matches).toEqual(1);
-    });
-});
-
-describe('the grid-container grid area', () => {
-    it('should have a header that extends horizontally across the top row', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match( /\.grid-container.*{[\s\S][^}]*display:.*grid.*;[\s\S][^}]*grid-template-areas.*:[\s\S][^}]*'leftNav header/g).length;
-      });
-      
-      expect(matches).toEqual(1);
-    });
-});
-
-describe('the grid-container grid area', () => {
-    it('should have a header that is twice the width of the left navigation bar', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match( /\.grid-container.*{[\s\S][^}]*display:.*grid.*;[\s\S][^}]*grid-template-areas.*:[\s\S][^}]*'leftNav header header'/g).length;
-      });
-      
-      expect(matches).toEqual(1);
-    });
-});
-
-describe('the grid-container grid area', () => {
-    it('should have a main section that extends horizontally across the second row', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match( /\.grid-container.*{[\s\S][^}]*display:.*grid.*;[\s\S][^}]*grid-template-areas.*:[\s\S][^}]*'leftNav header header'[\s\S]*'leftNav main/g).length;
-      });
-      
-      expect(matches).toEqual(1);
-    });
-});
-
-describe('the grid-container grid area', () => {
-    it('should have a main section that is twice the width of the left navigation bar', async () => {
-      const matches = await page.$eval('style', (style) => {
-        return style.innerHTML.match( /\.grid-container.*{[\s\S][^}]*display:.*grid.*;[\s\S][^}]*grid-template-areas.*:[\s\S][^}]*'leftNav header header'[\s\S][^}]*'leftNav main main'.*;/g).length;
-      });
-      
-      expect(matches).toEqual(1);
-    });
+    expect(result).toBe('39');
+  });
 });
